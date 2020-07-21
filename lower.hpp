c53d42ac975822dd8e4bcd69f6d4c18d60a51dd9
naynay18 <naomi1899@yahoo.com>
2020-07-20 20:01:46 -0700

    updated lower file

--- lower.hpp
+++ lower.hpp
@@ -2,20 +2,32 @@
 #define __LOWER_HPP__
 
 #include <cctype>
-#include “baseDecorator.hpp”
+#include <cstring>
+#include <cstdio>
+#include <iostream>
+#include "baseDecorator.hpp"
 
 class Lower : public BaseDecorator {
     private:
         Base* x;
     public:
-        Lower() : BaseDecorator(Base* src) { }
+        Lower() : BaseDecorator(x) { }
 
         virtual void display() { 
-		x.getString();
-	       	for (int i = 0; i < strlen(x); i++) {
-                        //putchar(tolower(x[i]));
+		/*char* str[] = x->getString();
+		for (int i = 0; i < str.size(); i++) {
+                        //putchar(tolower(str[i]));
 			std::cout << std::putchar(tolower(x[i])) << std::endl; 
-                }
+                }*/
+		int j = 0;
+		std::string str = x->getString();
+		//char* str = x->getString();
+		char ch = str[0];
+		while (str[j]) {
+			ch = str[j];
+			putchar(tolower(ch));
+			j++;
+		}
 	}
 };
 
